10a36284d659749fef32a90fcbfe23be28c56bad
Daniel Guirra <daniel.guirra777@gmail.com>
2023-10-31 15:08:55 -0300

    Tue Oct 31 2023 15:08:55 GMT-0300 (Horário Padrão de Brasília) + BACKUP

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 const simpleGit = require('simple-git');
 
-const fs = require('fs')
-
-
-const options = {
-   baseDir: process.cwd(),
-   binary: 'git',
-   maxConcurrentProcesses: 6,
-   trimmed: false,
-};
-
-
-
-
 
 simpleGit()
    .init()
    .add('./*')
-   .commit('first commit!')
+   .commit(`${new Date()} + BACKUP`)
    .push()
